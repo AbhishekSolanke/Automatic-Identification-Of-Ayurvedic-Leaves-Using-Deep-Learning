81629c0a0f917d0387a9704429de59c7520e79ed
Abhishek Solanke <103139759+AbhishekSolanke@users.noreply.github.com>
2022-04-08 20:05:46 +0530

    update lp1.py

--- lp1.py
+++ lp1.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 from keras.preprocessing.image import load_img,img_to_array
 import numpy as np
+from keras.preprocessing import image as image_utils
+from keras.preprcessing.text import Toknizer
 from keras.models import load_model
 import requests
 from streamlit_lottie import st_lottie
